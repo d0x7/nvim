7b5fcb3d3a6e0c6c04def694d140d71ec50ee7a0
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2025-02-19 17:38:45 +0100

    fix: update bufferline config

--- lua/optionals/bufferline.lua
+++ lua/optionals/bufferline.lua
@@ -2,8 +2,27 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function(_, opts)
-		require("bufferline").setup({})
+	event = "VeryLazy",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				mode = "tabs",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true, -- Adds a separator between Neo-tree and buffers
+					},
+				},
+				diagnostics = "nvim_lsp",
+				separator_style = { "", "" },
+				modified_icon = "●",
+				show_close_icon = false,
+				show_buffer_close_icons = true,
+			},
+		})
 	end,
 	keys = {
 		{ "<C-w><C-w>", "<cmd>bdelete<cr>", mode = { "n" }, opts = { noremap = true }, desc = "Close current buffer." },
